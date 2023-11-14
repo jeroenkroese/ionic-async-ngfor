f849c2f78b20b5ed03d4e7e6e4c3303d41abc271
Jeroen Kroese <jeroen@pix.nl>
2023-11-14 11:53:50 +0100

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,34 @@
+import { AsyncPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+	IonHeader,
+	IonToolbar,
+	IonTitle,
+	IonContent,
+	IonPopover,
+	IonButton,
+} from '@ionic/angular/standalone';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
+	standalone: true,
+	imports: [
+		IonHeader,
+		IonToolbar,
+		IonTitle,
+		IonContent,
+		IonPopover,
+		IonButton,
+		NgFor,
+		AsyncPipe,
+	],
 })
 export class HomePage {
-  constructor() {}
+	items$: Observable<string[]>;
+	constructor() {
+		this.items$ = of(['item1', 'item2']).pipe(delay(100));
+	}
 }
